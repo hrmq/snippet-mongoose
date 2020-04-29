c072e6de03e4280a9370b7f1509b15d6aa72078c
Hasan R <h.rahimi.moqadam@gmail.com>
2020-04-29 17:10:15 +0000

    add mongoose connection

---
+++ connect.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const mongoUrl = 'mongodb://localhost/mydatabase';
+
+mongoose.connect(mongoUrl);
--- index.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUrl = 'mongodb://localhost/mydatabase';
-
-mongoose.connect(mongoUrl);
